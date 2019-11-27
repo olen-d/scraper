12549630365502c247ffa8171836ba6798b4a410
Salvador Dieffenbach <zogwarg@hotmail.com>
2019-11-26 23:16:04 -0500

    Modal for displaying existing and soliciting new notes implemented

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,5 +1,4 @@
 const saveClassName = document.getElementsByClassName("saveBtn");
-const removeClassName = document.getElementsByClassName("removeBtn");
 
 // TODO: Refactor this to change the functions to methods and export an object
 const saveArticle = function() {
@@ -22,30 +21,6 @@ const saveArticle = function() {
   });
 }
 
-const removeArticle = function() {
-  const articleId = this.getAttribute("data-article-id");
-
-  axios.delete("/article/remove", {
-    data: { articleId }
-  })
-  .then(response => {
-    if (response.status === 200) {
-
-      //Delete teh box with the data id from the dom
-      let elem = document.getElementById(`js-${response.data}`);
-      elem.parentNode.removeChild(elem);
-    }
-  })
-  .catch(error => {
-    console.log(error);
-    alert(error);
-  })
-}
-
 Array.from(saveClassName).forEach(e => {
   e.addEventListener("click", saveArticle);
-});
-
-Array.from(removeClassName).forEach(e => {
-  e.addEventListener("click", removeArticle);
 });
